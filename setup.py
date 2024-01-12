90f4f88e617e9d16ef8665859b7b2f792e45ad4e
Paweł Knioła <pawel.kn@gmail.com>
2024-01-12 17:31:09 +0100

    Update setup file

--- setup.py
+++ setup.py
@@ -4,17 +4,21 @@ setup(
     name="btlib",
     author='Paweł Knioła',
     author_email='pawel.kn@gmail.com',
-    description='Backtest library focused on portfolio testing',
+    description='Python backtest library optimized for testing portfolio performance across hundreds of tickers',
     long_description=open('README.md', encoding='utf-8').read(),
     license='MIT',
-    keywords='quantitative analysis backtesting parallel algorithmic trading',
-    url='https://github.com/pawelkn/backtester',
+    keywords='python python3 quantitative analysis backtesting portfolio parallel algorithmic trading',
+    url='https://github.com/pawelkn/btlib',
     classifiers=[
-        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11"
     ],
-    python_requires='>=3.5',
-
+    python_requires='>=3.7',
     version="0.0.1",
     packages=['btlib'],
 )
